b03d4f120a54cf70e74141bacd1f42d6862f7eec
Kat Law <kathleenmlaw1@gmail.com>
2024-05-25 23:01:35 -0400

    lol reading from wrong register endpoint

--- app/register/handleRegister.tsx
+++ app/register/handleRegister.tsx
@@ -4,7 +4,7 @@ export const handleRegister = async (data: FormData) => {
   const password = data.get('password');
 
   try {
-    const response = await fetch('http://localhost:3000/api/auth/register', {
+    const response = await fetch(`http://${process.env.NEXTAUTH_URL}/api/auth/register`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
